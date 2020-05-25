68f6c66713866ef4138d1f26a0b334e633b1cd08
Jessica Thurman <jaithurman@gmail.com>
2020-05-25 15:12:38 -0500

    attempt three

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+function createATimeline(props){
+    return <div>create a timeline {props.name}</div>
+ }
+
+ function getAnAccountabilityPartner(props){
+    return <div>get an accountability partner {props.name}</div>
+ }
+
+ function followDailyWritingPrompts(props){
+    return <div>follow daily writing prompts {props.name}</div>
+ }
+
+ function writeAChapterAWeek(props){
+    return <div>write a chapter a week {props.name}</div>
+ }
+
+ function readABookThatWillInspireYou(props){
+    return <div>Read a book that will inspire you {props.name}</div>
+ }
+
+ function practiceBreathing(props){
+    return <div>practice breathing {props.name}</div>
+ }
+
+ 
